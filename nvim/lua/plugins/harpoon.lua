3095d488482e445bbdb51a54d850d5ce4a4a65ea
Nathan Budd <nathan@audette.io>
2024-04-14 19:42:03 -0400

    update a harpoon keymap

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -6,7 +6,7 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>h", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
