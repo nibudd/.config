08a191ec2bf887a86240126dc25179340a071c36
Nathan Budd <nathan@audette.io>
2024-05-03 08:43:02 -0400

    add quick reference to dark/light themes for catppuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,7 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme "catppuccin-frappe"
+		-- vim.cmd.colorscheme "catppuccin-latte" -- light
+		vim.cmd.colorscheme "catppuccin-frappe" -- dark
 	end
 }
