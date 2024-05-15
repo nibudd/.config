470cdf6f4e6513b36f6b68fe10db19c2de479d13
Nathan Budd <nathan@audette.io>
2024-05-15 09:03:50 -0400

    add daily updates to Lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,14 +3,20 @@ require("config")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
-    	"git",
-    	"clone",
-    	"--filter=blob:none",
-    	"https://github.com/folke/lazy.nvim.git",
-    	"--branch=stable", -- latest stable release
-    	lazypath,
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+    checker = {
+        enabled = true,
+        frequency = 60 * 60 * 24 -- every 24hrs
+    }
+}
+)
