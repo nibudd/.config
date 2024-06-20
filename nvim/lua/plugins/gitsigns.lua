67c81be6bb4f2a3cc6e1ddfe0a2563079eb5bc5e
Nathan Budd <nathan@audette.io>
2024-06-20 16:02:39 -0400

    remove deprecated option

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -29,9 +29,6 @@ return {
                 virt_text_priority = 100,
             },
             current_line_blame_formatter = '<author>, <author_time:%y-%m-%d@%H:%M> #<abbrev_sha> - <summary>',
-            current_line_blame_formatter_opts = {
-                relative_time = true,
-            },
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
