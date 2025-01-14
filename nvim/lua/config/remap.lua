abffcacfba6bfeb4ae3cb2fe79eb0673b947b699
Nathan Budd <nibudd@gmail.com>
2025-01-14 15:05:44 -0500

    set up leap

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -55,4 +55,8 @@ vim.keymap.set("n", "<leader>%",
     end,
     { desc = "yank absolute filepath to register f" }
 )
-
+ 
+vim.keymap.set('n',        's', '<Plug>(leap)')
+vim.keymap.set('n',        'S', '<Plug>(leap-from-window)')
+vim.keymap.set({'x', 'o'}, 's', '<Plug>(leap-forward)')
+vim.keymap.set({'x', 'o'}, 'S', '<Plug>(leap-backward)')
