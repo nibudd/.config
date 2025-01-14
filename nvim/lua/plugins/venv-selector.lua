d0e9fbe2ac2e29e2de3a925180bb547489f93d87
Nathan Budd <nibudd@gmail.com>
2025-01-14 14:41:45 -0500

    remove deprecated venv-selector code

--- nvim/lua/plugins/venv-selector.lua
+++ nvim/lua/plugins/venv-selector.lua
@@ -1,27 +1,3 @@
--- x = {
---     'linux-cultist/venv-selector.nvim',
---     dependencies = {
---         'neovim/nvim-lspconfig',
---         'nvim-telescope/telescope.nvim',
---         'mfussenegger/nvim-dap-python',
---         'microsoft/debugpy',
---         'mfussenegger/nvim-dap',
---     },
---     opts = {
---         auto_refresh = true,
---         dap_enabled = true,
---         name = {"venv", ".venv"},
---         search_venv_managers = false,
---     },
---     -- event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
---     keys = {
---         -- Keymap to open VenvSelector to pick a venv.
---         { '<leader>vs', '<cmd>VenvSelect<cr>' },
---         -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
---         { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
---     },
--- }
-
 return {
     "linux-cultist/venv-selector.nvim",
     dependencies = {
