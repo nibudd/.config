b1cdb698b461344a786515812912ee7f1468bff9
Nathan Budd <nibudd@gmail.com>
2026-01-27 11:28:06 -0500

    fix plugin loading

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,11 @@
+-- Import all plugin subdirectories
+return {
+    { import = "plugins.editing" },
+    { import = "plugins.git" },
+    { import = "plugins.lsp" },
+    { import = "plugins.navigation" },
+    { import = "plugins.syntax" },
+    { import = "plugins.testing" },
+    { import = "plugins.tools" },
+    { import = "plugins.ui" },
+}
