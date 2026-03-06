46faf17bf15532940c4cc1e713a29c09b672e9c4
Nathan Budd <nibudd@gmail.com>
2026-03-06 11:26:06 -0500

    new binding for updating/upgrading brew

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -2,4 +2,5 @@
 # ln -s ~/.config/omz/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 alias gs="git status"
 alias dad="curl -H 'Accept: application/json' https://icanhazdadjoke.com/ | jq '.joke'"
+alias bubu="brew update && brew upgrade"
 
