1e6662a0a0cedac3d0129aa9d6a8ab157b3d0a90
Nathan Budd <nibudd@gmail.com>
2026-03-12 10:42:21 -0400

    undo changes to markdown language file. md renderers prefer 4-space tabs

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,10 +1,6 @@
 vim.opt_local.wrap = true       -- Enable visual wrapping
 vim.opt_local.linebreak = true  -- Wrap at word boundaries rather than in the middle of a word
 vim.opt_local.breakindent = true -- Maintain indentation on wrapped lines
-vim.bo.expandtab = true
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
-vim.bo.tabstop = 2
 
 -- remap j/k to navigate by screen lines (gj/gk)
 vim.keymap.set("n", "j", "gj", { buffer = true })
