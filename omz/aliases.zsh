6055f455742d44266e396c44ccb6a73e26677dc1
Nathan Budd <nibudd@gmail.com>
2026-05-05 15:31:07 -0400

    add gcloud updating to `update` alias

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -2,5 +2,10 @@
 # ln -s ~/.config/omz/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
 alias gs="git status"
 alias dad="curl -H 'Accept: application/json' https://icanhazdadjoke.com/ | jq '.joke'"
-alias bubu="brew update && brew upgrade"
+update() {
+  echo '> brew update'   && brew update   && \
+  echo '> brew upgrade'  && brew upgrade  && \
+  echo '> claude update' && claude update && \
+  echo '> gcloud components update' && gcloud components update
+}
 
