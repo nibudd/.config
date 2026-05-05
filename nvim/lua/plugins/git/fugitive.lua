0bb13f3ab651988c7a57f33d9fe978fe4f32ccac
Nathan Budd <nibudd@gmail.com>
2026-05-05 15:33:08 -0400

    fix open-ended fugitive prompt

--- nvim/lua/plugins/git/fugitive.lua
+++ nvim/lua/plugins/git/fugitive.lua
@@ -2,7 +2,7 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<leader>gg", "<cmd>Git ")
+		vim.keymap.set("n", "<leader>gg", ":Git ")
 		vim.keymap.set("n", "<leader>gj", "<cmd>Git <cr>")
 	end,
 }
