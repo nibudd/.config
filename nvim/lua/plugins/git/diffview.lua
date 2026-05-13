28b1455ae12c7bbd3fe3c54a18a8a82ef8614405
Nathan Budd <nibudd@gmail.com>
2026-05-13 14:53:06 -0400

    using diffview is a bit easier with my preferred layout and a keymap

--- nvim/lua/plugins/git/diffview.lua
+++ nvim/lua/plugins/git/diffview.lua
@@ -1,2 +1,13 @@
 -- side-by-side git diff viewer
-return { "sindrets/diffview.nvim" }
+return {
+    "sindrets/diffview.nvim",
+    config = function()
+        require("diffview").setup({
+            view = {
+                merge_tool = {
+                    layout = "diff3_mixed",
+                },
+            },
+        })
+    end,
+}
