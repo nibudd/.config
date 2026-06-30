d6e83af4ffa3d1f4f98917c054998de6dd99c1ac
Nathan Budd <nibudd@gmail.com>
2026-06-30 06:56:01 -0400

    add sudo to brew upgrade

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -4,7 +4,7 @@ alias gs="git status"
 alias dad="curl -H 'Accept: application/json' https://icanhazdadjoke.com/ | jq '.joke'"
 update() {
   echo '> brew update'   && brew update   && \
-  echo '> brew upgrade --no-ask'  && brew upgrade --no-ask  && \
+  echo '> sudo brew upgrade --no-ask'  && sudo brew upgrade --no-ask  && \
   echo '> claude update' && claude update && \
   echo '> gcloud components update' && gcloud components update
 }
