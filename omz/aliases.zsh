6a849945ca138fa7559c5613d47a7e975eb01c8f
Nathan Budd <nibudd@gmail.com>
2026-07-17 10:47:50 -0400

    update aliases (allow for aliases that come with omz)

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -1,10 +1,3 @@
-# make a symlink of this file in your zsh custom directory
-# ln -s ~/.config/omz/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh
-gs()  { echo '> git status'       && git status; }
-gf()  { echo '> git fetch'        && git fetch; }
-gp()  { echo '> git push'         && git push; }
-gm()  { echo "> git merge $*"     && git merge "$@"; }
-gmo() { echo '> git merge origin/main' && git merge origin/main; }
 alias dad="curl -H 'Accept: application/json' https://icanhazdadjoke.com/ | jq '.joke'"
 update() {
   echo '> brew update'   && brew update   && \
